c3dd529f62192e2ec3b6495c737b9774db06b3ce
krboswell <krboswell@gmail.com>
2012-02-08 22:45:23 -0600

    First stab at Android Client GUI elements

--- sabacc-android3-client/src/main/java/com/clementscode/SabaccClientActivity.java
+++ sabacc-android3-client/src/main/java/com/clementscode/SabaccClientActivity.java
@@ -3,6 +3,7 @@ package com.clementscode;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RelativeLayout;
 
 public class SabaccClientActivity extends Activity {
 
@@ -18,7 +19,7 @@ public class SabaccClientActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		Log.i(TAG, "onCreate");
-        setContentView(R.layout.main);
+        setContentView(R.layout.table_layout);
     }
 
 }
